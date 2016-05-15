b483e28b6cd3ee2efb1385cc76180233ed2ee939
tallistroan <tallistroan@gmail.com>
2016-05-15 10:41:12 +0200

    General change to mapbox gl js, create global options file

---
+++ options.js
@@ -0,0 +1,8 @@
+// mapbox API Key for displaying the map
+var mapbox_accesstoken = 'mapboxAPIKey';
+
+// provide boundaries if you wish, that the user is only able to pan and zoom at your city and not at the whole world
+// but choose the corners generous, because otherwise the user is very limited in the zoom-functionality especially at small zoom levels
+var southEastBounds = new mapboxgl.LngLat(10.754076,47.653085);
+var northWestBounds = new mapboxgl.LngLat(12.649536,48.756453);
+var bounds = new mapboxgl.LngLatBounds(southEastBounds, northWestBounds);
